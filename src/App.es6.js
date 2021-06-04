698203e025d5f74e157096ce925d92da804142d5
Nthabi <mshnth009@myuct.ac.za>
2021-06-04 16:24:38 +0200

    added some styling

--- src/App.es6.js
+++ src/App.es6.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import { Button, FormControl, FormGroup, InputGroup} from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
     render() {
         return(
-            <div>
+            <div className="container">
                 <title>Music app</title>
-                <input placeholder="Search an artist"/>
-                <button>Search</button>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type="text" placeholder="Search for an artist"/>
+                    </InputGroup>
+                    {/* <InputGroup.Addon>
+                        <Glyphicon glyph="search"></Glyphicon>
+                    </InputGroup.Addon> */}
+                </FormGroup>
+                <Button variant="outline-dark">Search</Button>
                 <div className="profile">
                     <div>Artist picture</div>
                     <h5>Artist name</h5>

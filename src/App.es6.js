b3b6f05d42d369bd0f7e5e34f26585bee51adc8b
Nthabi <mshnth009@myuct.ac.za>
2021-06-05 19:39:50 +0200

    search functionality working with axios, currentky returning one element

--- src/App.es6.js
+++ src/App.es6.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Button, FormControl, FormGroup, InputGroup} from 'react-bootstrap';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -12,9 +13,26 @@ class App extends Component {
 
     search() {
         
-        const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1&access_token=BQCtXzoXGWEbUynSsgjlo8TK-LnzRHci3nThYKXQlNDfQh1rTu0lmA8riMsFxF7lEwYGR4shxeiDsSDQrawd7uolgNHVUYS4v8iplYyduiadMvdDiTZWd60_NLQgMXXedz4sCslITNkOPXFg4kXTaxfiq8HBP6w';
-        console.log('url: ', FETCH_URL);
+        // const BASE_URL = 'https://api.spotify.com/v1/search?';
+        // const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1&access_token=BQCtXzoXGWEbUynSsgjlo8TK-LnzRHci3nThYKXQlNDfQh1rTu0lmA8riMsFxF7lEwYGR4shxeiDsSDQrawd7uolgNHVUYS4v8iplYyduiadMvdDiTZWd60_NLQgMXXedz4sCslITNkOPXFg4kXTaxfiq8HBP6w`;
+        // console.log('url: ', FETCH_URL);
+        // const BASE_URL ='https://api.spotify.com/v1/search?';
+        // const acccessToken = 'BQAvXeTwWORXBfTAIDCcHBLlTyHUZ9pShNFjzd48gNFz-TN2GPH8MrxXQimM_oKDYg3GmGVfkGLsB0PaMXrlbm7myTyme2e_7x2dGs8dI1HW1OhFnzjmjh-WilKymp63ue1j8JCkdbnkSlV_TAcUPcHM1nnC8DI';
+
+        // const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1&${acccessToken}`;
+        // console.log('FETCH_URL', FETCH_URL);
+       
+       
+        const options = {
+            url: `https://api.spotify.com/v1/search?q=${this.state.query}&type=artist&limit=1&access_token=BQAvXeTwWORXBfTAIDCcHBLlTyHUZ9pShNFjzd48gNFz-TN2GPH8MrxXQimM_oKDYg3GmGVfkGLsB0PaMXrlbm7myTyme2e_7x2dGs8dI1HW1OhFnzjmjh-WilKymp63ue1j8JCkdbnkSlV_TAcUPcHM1nnC8DI`,
+            method: 'GET'
+          };
+          
+          axios(options)
+            .then(response => {
+              console.log(response.status);
+              console.log(options.url);
+            });
         
     }
 

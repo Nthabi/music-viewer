904b3c30d8bc84b2fa6e622e2405d63d84f0c1a1
Nthabi <mshnth009@myuct.ac.za>
2021-06-09 14:25:44 +0200

    adding logic to dispaly artist tracks in gallery

--- src/components/gallery/Gallery.es6.js
+++ src/components/gallery/Gallery.es6.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 
 class Gallery extends Component {
     render() {
+        const {tracks} = this.props;
         return(
-            <div>Gallery</div>
+            <div>Gallery
+                {
+                    tracks.map((track, key) => {
+                        const trackImg = track.album.images[0].url;
+                        return (
+                            <div key={k} className="track">
+                                <img src={trackImg} className="track-img" alt="track"/>
+                                <p className="track-text"> {track.name} </p>
+                            </div>
+                        )   
+                    })
+                }
+            </div>
         )
     }
 
